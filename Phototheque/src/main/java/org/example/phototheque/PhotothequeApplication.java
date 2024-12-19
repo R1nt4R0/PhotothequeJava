55dc94a0f9525be20137fbb97392dfc591731c09
Vladuyinn <romaincrombetravail@gmail.com>
2024-12-19 15:36:10 +0100

    Ajout dépendances projet Java

---
+++ Phototheque/src/main/java/org/example/phototheque/PhotothequeApplication.java
@@ -0,0 +1,13 @@
+package org.example.phototheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotothequeApplication.class, args);
+    }
+
+}
